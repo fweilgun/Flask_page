e2a83242afa64b06e9cd858db4d815703d1b2198
Weilguny Fabian <fweilgun@gmail.com>
2023-05-19 12:04:51 +0200

    UI-Verändert (Button hinzugefügt bzw. Firebase-C.)

--- app.py
+++ app.py
@@ -1,12 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from firebase_admin import db, credentials, initialize_app
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template('index.html')
+    i = False
+    if request.form.get("action") == "Senden":
+        data = request.form.get("Time")
+        ref = db.reference("/")
+        ref.update({"active": i})
+        ref.update({"Time": int(data)})
+
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
+    cred_obj = credentials.Certificate(
+        "esp32-uvhaerter-firebase-adminsdk-ao16c-90b83e6e37.json"
+    )
+    default_app = initialize_app(
+        cred_obj,
+        {
+            "databaseURL": "https://esp32-uvhaerter-default-rtdb.europe-west1.firebasedatabase.app/"
+        },
+    )
     app.run(debug=True)
